fc3fc5fdc8d3f6a3b20db88181c1f37c89392d57
Cyrille DEGASQUET <pro.cyrille.degasquet@gmail.com>
2025-02-26 14:48:59 +0100

    Ajout de la route par défaut

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "games#new"
   Rails.application.routes.draw do
     get 'new', to: 'games#new'
     post 'score', to: 'games#score'
